5902a2973c8ef1a817d912ab7ec00c36e6cbed05
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-04-18 20:32:28 -0400

    comment out calc, alias "calc" to google calculator

--- modules/calc.py
+++ modules/calc.py
@@ -27,45 +27,45 @@ subs = [
    ('mbps', '(megabits / second)')
 ]
 
-def calc(phenny, input): 
-   """Use the Frink online calculator."""
-   q = input.group(2)
-   if not q: 
-      return phenny.say('0?')
+#def calc(phenny, input): 
+   #"""Use the Frink online calculator."""
+   #q = input.group(2)
+   #if not q: 
+      #return phenny.say('0?')
 
-   query = q[:]
-   for a, b in subs: 
-      query = re.sub(a, b, query)
-   query = query.rstrip(' \t')
+   #query = q[:]
+   #for a, b in subs: 
+      #query = re.sub(a, b, query)
+   #query = query.rstrip(' \t')
 
-   precision = 5
-   if query[-3:] in ('GBP', 'USD', 'EUR', 'NOK'): 
-      precision = 2
-   query = web.urllib.quote(query.encode('utf-8'))
+   #precision = 5
+   #if query[-3:] in ('GBP', 'USD', 'EUR', 'NOK'): 
+      #precision = 2
+   #query = web.urllib.quote(query.encode('utf-8'))
 
-   uri = 'http://futureboy.us/fsp/frink.fsp?fromVal='
-   bytes = web.get(uri + query)
-   m = r_result.search(bytes)
-   if m: 
-      result = m.group(1)
-      result = r_tag.sub('', result) # strip span.warning tags
-      result = result.replace('&gt;', '>')
-      result = result.replace('(undefined symbol)', '(?) ')
+   #uri = 'http://futureboy.us/fsp/frink.fsp?fromVal='
+   #bytes = web.get(uri + query)
+   #m = r_result.search(bytes)
+   #if m: 
+      #result = m.group(1)
+      #result = r_tag.sub('', result) # strip span.warning tags
+      #result = result.replace('&gt;', '>')
+      #result = result.replace('(undefined symbol)', '(?) ')
 
-      if '.' in result: 
-         try: result = str(round(float(result), precision))
-         except ValueError: pass
+      #if '.' in result: 
+         #try: result = str(round(float(result), precision))
+         #except ValueError: pass
 
-      if not result.strip(): 
-         result = '?'
-      elif ' in ' in q: 
-         result += ' ' + q.split(' in ', 1)[1]
+      #if not result.strip(): 
+         #result = '?'
+      #elif ' in ' in q: 
+         #result += ' ' + q.split(' in ', 1)[1]
 
-      phenny.say(q + ' = ' + result[:350])
-   else: phenny.reply("Sorry, can't calculate that.")
-   phenny.say('Note that .calc is deprecated, consider using .c')
-calc.commands = ['calc']
-calc.example = '.calc 5 + 3'
+      #phenny.say(q + ' = ' + result[:350])
+   #else: phenny.reply("Sorry, can't calculate that.")
+   #phenny.say('Note that .calc is deprecated, consider using .c')
+#calc.commands = ['calc']
+#calc.example = '.calc 5 + 3'
 
 def c(phenny, input): 
   """Google calculator."""
@@ -89,7 +89,7 @@ def c(phenny, input):
     answer = web.decode(answer)
     phenny.say(answer)
   else: phenny.say('Sorry, no result.')
-c.commands = ['c']
+c.commands = ['c','calc']
 c.example = '.c 5 + 3'
 
 def py(phenny, input): 
