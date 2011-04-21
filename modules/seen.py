b50a89ce1eafd442ae927b68406fa29d26cd6340
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-04-20 22:00:22 -0400

    added ability for BotSteve to recognize own seen command, added 'seen kyle'

--- modules/seen.py
+++ modules/seen.py
@@ -12,19 +12,23 @@ from tools import deprecated
 
 @deprecated
 def f_seen(self, origin, match, args): 
-   """.seen <nick> - Reports when <nick> was last seen."""
-   if origin.sender == '#talis': return
-   nick = match.group(2).lower()
-   if not hasattr(self, 'seen'): 
+  """.seen <nick> - Reports when <nick> was last seen."""
+  if origin.sender == '#talis': return
+  nick = match.group(2).lower()
+  if nick == "kyle":
+    return self.msg(origin.sender, "He's about this tall?  Seen Kyle?")
+  if nick == "botsteve":
+    return self.msg(origin.sender, "I'm right here, actually.")
+  if not hasattr(self, 'seen'): 
       return self.msg(origin.sender, '?')
-   if self.seen.has_key(nick): 
+  if self.seen.has_key(nick): 
       channel, t = self.seen[nick]
       t = time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime(t))
 
       msg = "I last saw %s at %s on %s" % (nick, t, channel)
       self.msg(origin.sender, str(origin.nick) + ': ' + msg)
-   else: self.msg(origin.sender, "Sorry, I haven't seen %s around." % nick)
-f_seen.rule = (['seen'], r'(\S+)')
+  else: self.msg(origin.sender, "Sorry, I haven't seen %s around." % nick)
+f_seen.rule = (['seen', 'lastseen'], r'(\S+)')
 
 @deprecated
 def f_note(self, origin, match, args): 
