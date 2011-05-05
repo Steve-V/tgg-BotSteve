273d56a350c65710a944ed521818bb6070b0e794
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-05-05 00:34:09 -0400

    add should i to ping

--- modules/ping.py
+++ modules/ping.py
@@ -8,10 +8,10 @@ About: http://inamidst.com/phenny/
 import random
 
 def hello(phenny, input): 
-   greeting = random.choice(('Hi', 'Hey', 'Hello', 'Word up', 'Greetings', 'Yo', 'Cheers'))
+   greeting = random.choice(('Hi', 'Hey', 'Hello', 'Word up', 'Greetings', 'Yo', 'Cheers', 'Salutations'))
    punctuation = random.choice(('', '!'))
    phenny.say(greeting + ' ' + input.nick + punctuation)
-hello.rule = r'(?i)(hi|hello|hey|greetings|ohai|ohi|hai|sup) (?i)$nickname\b'
+hello.rule = r'(?i)(hi|hello|hey|greetings|ohai|ohi|hai|sup|salutations) (?i)$nickname\b'
 
 def interjection(phenny, input): 
    phenny.say(input.nick + '!')
@@ -32,6 +32,13 @@ def amiright(phenny, input):
 amiright.rule = r'(?i)(right|correct|is that right|am i right) (?i)$nickname\b'
 amiright.priority = 'high'
 amiright.thread = False
+
+def shouldI(phenny, input):
+  reply = random.choice( ("Yep!", "Oh yeah!", "Definitely", "Absolutely", "Nope", "No way", "Not a chance", "I don't think so", "Maybe", "Ask again later!", "Reply hazy, try again later") )
+  phenny.say(reply)
+shouldI.rule = r'(?i)$nickname\b(?i)(, should i| should i|: should i)'
+shouldI.priority = 'high'
+shouldI.thread = False
 
 if __name__ == '__main__': 
    print __doc__.strip()
