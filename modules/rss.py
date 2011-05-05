b0445f985a62653a93e7f1c0dc374c134af94eae
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-05-04 23:06:39 -0400

    minor bugfixes

--- modules/rss.py
+++ modules/rss.py
@@ -12,7 +12,7 @@ Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.p
 
 #later we can make this use SQlite if we want
 #import sqlite3
-import time, threading, feedparser
+import time, threading, feedparser, gdata.youtube, gdata.youtube.service
 
 def setup(phenny): 
 
@@ -20,20 +20,29 @@ def setup(phenny):
     #set up the channel that messages will be transmitted to
     #FIXME
     #this should be read from a config file
-    whichChannel = '#thegeekgroup'
-    #pull original feed
+    mainChannel = '#tgg-bots'
+    testChannel = '#tgg-bots'
+    youtubeUserName = 'physicsduck'
+    
+    #pull original forum feed
     oldFeed = feedparser.parse("http://thegeekgroup.org/bb/?xfeed=all&feedkey=60635da5-d00a-4f9e-a007-a9102251b1c1")
+    
+    #pull original youtube feed
+    youtubeServe = gdata.youtube.service.YouTubeService()
+    youtubeUri = 'http://gdata.youtube.com/feeds/api/users/%s/uploads' % youtubeUserName
+    oldYoutubeFeed = youtubeServe.GetYouTubeVideoFeed(youtubeUri)
+    
     import time
     time.sleep(20)
     
     while True: 
       
-      #pull feed again
-      #phenny.msg(whichChannel, "Pulling new feed")
+      #pull forum feed again
+      phenny.msg(testChannel, "Pulling new video feeds")
       currentFeed = ''
       currentFeed = feedparser.parse("http://thegeekgroup.org/bb/?xfeed=all&feedkey=60635da5-d00a-4f9e-a007-a9102251b1c1")
       
-      #compare feeds
+      #compare forum feeds
       titlesOld = []
       titlesCurrent = []
       titlesChanged = []
@@ -57,14 +66,51 @@ def setup(phenny):
       
       #print the string only if there's something to output
       if titlesChanged:
-        phenny.msg(whichChannel, outputString)
+        phenny.msg(mainChannel, outputString)
         oldFeed = currentFeed #don't forget to update
       
-      #debugging
-      #else:
-        #phenny.msg(whichChannel, "No new feeds")
+      #=======================
       
-      #phenny.msg(whichChannel, "sleeping...")
+      #pull youtube feed again
+      currentYoutubeFeed = youtubeServe.GetYouTubeVideoFeed(youtubeUri)
+      
+      #compare forum feeds
+      youtubeURLsOld = []
+      youtubeTitlesCurrent = []
+      youtubeTitlesChanged = []
+      
+      for items in oldYoutubeFeed.entry:
+        youtubeURLsOld.append( str( items.GetSwfUrl() ).split("?")[0] )
+      for items in currentYoutubeFeed.entry:
+        youtubeTitlesCurrent.append( (items.media.title.text, str( items.GetSwfUrl() ).split("?")[0] ) )
+      
+      for title,url in youtubeTitlesCurrent:
+        if url not in youtubeURLsOld:
+          youtubeTitlesChanged.append(title, url)
+      
+      #rebuild the output string
+      if youtubeTitlesChanged:
+        outputString = 'In the last hour, there have been '
+        outputString += str( len(youtubeTitlesChanged) )
+        outputString += " new YouTube videos posted by The Geek Group.  New videos: "
+        #print the header
+        phenny.msg(testChannel, outputString)
+        
+        #print the videos
+        for eachTitle, eachURL in youtubeTitlesChanged:
+          outputString = eachTitle
+          outputString += " "
+          outputString += eachURL
+          phenny.msg(testChannel, outputString)
+        
+        #update to the new feed
+        oldYoutubeFeed = currentYoutubeFeed
+      
+      #debugging
+      else:
+        phenny.msg(testChannel, "No new feeds")
+      
+      #phenny.msg(testChannel, "sleeping...")
       import time
       time.sleep(3600)
   
