ca42e08d649377dcbd03cbf59a8e04839f4221c0
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-05-16 01:41:32 -0400

    implemented persistent seen database.  fixes #33

--- modules/seen.py
+++ modules/seen.py
@@ -11,22 +11,26 @@ import time
 from tools import deprecated
 from decimal import *
 import os
+import pickle
 
 def setup(self): 
-   fn = self.nick + '-' + self.config.host + '.seen.db'
-   self.seen_filename = os.path.join(os.path.expanduser('~/.phenny'), fn)
-   if not os.path.exists(self.seen_filename): 
-      try: f = open(self.seen_filename, 'w')
-      except OSError: pass
-      else: 
-         f.write('')
-         f.close()
-   #unpickle module here
-
+  fn = self.nick + '-' + self.config.host + '.seen.db'
+  self.seen_filename = os.path.join(os.path.expanduser('~/.phenny'), fn)
+  if not os.path.exists(self.seen_filename):
+    try: f = open(self.seen_filename, 'w')
+    except OSError: pass
+    else: 
+      f.write('')
+      f.close()
+  self.seen = pickle.load( open(self.seen_filename) )
 
 @deprecated
 def f_seen(self, origin, match, args): 
   """.seen <nick> - Reports when <nick> was last seen."""
+  
+  #pickle datastore to file
+  pickle.dump(self.seen, open(self.seen_filename,'wb') )
+  
   if origin.sender == '#talis': return
   nick = match.group(2).lower()
   
@@ -38,8 +42,7 @@ def f_seen(self, origin, match, args):
   
   #error check
   if not hasattr(self, 'seen'): 
-      return self.msg(origin.sender, '?')
-  
+      return self.msg(origin.sender, 'Database Error!')
   
   if self.seen.has_key(nick): 
       channel, storedTime = self.seen[nick]
