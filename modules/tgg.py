2c45f63b2f89cdaa9dcb91cd4222e8db10fb32ff
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-09-01 01:45:40 -0400

    spelling errors in the sandwhich module

--- modules/tgg.py
+++ modules/tgg.py
@@ -214,29 +214,31 @@ def give_skittles(phenny, input):
 give_skittles.commands = ['skittles']
 give_skittles.priority = 'medium'
 
-def give_sandwhich(phenny, input):
-  #if it's the bot getting the sandwhich, or if the user is sandwhiching themselfs
+def give_sandwich(phenny, input):
+  #if it's the bot getting the sandwich, or if the user is sandwiching themselfs
   #code here
   if input.group(2):
-    if input.group(3).lower() == phenny.nick.lower():
+    if input.group(2).lower() == phenny.nick.lower():
       return(phenny.say("For me?  Thank you!  **gloms down and does not leave a single crumb on the plate.**") )
     else:
-      recepient = input.group(2) 
-  #otherwise give specified user sandwhich
+      recepient = input.group(2)
+  else:
+    recepient = str(input.nick)
+  #otherwise give specified user sandwich
   db_conn = sqlite3.connect("tgg.db")
   db_curr = db_conn.cursor()
   #nick = str(input.nick)
   
-  db_curr.execute( "SELECT * FROM sandwhich_type;" )
+  db_curr.execute( "SELECT * FROM sandwich_type;" )
   db_result = db_curr.fetchall()
   
   if (db_result):
     #first choose a db_result at random, then take element 1 of that result (the insult itself)
     flavor = random.choice(db_result)[1]
     
-  phenny.say("here you go, %s, I made you a %s sandwhich!" % (recepient, str(flavor) ) )
-give_sandwhich.commands = ['sandwhich']
-give_sandwhich.priority = 'medium'
+  phenny.say("here you go, %s, I made you a %s sandwich!" % (recepient, str(flavor) ) )
+give_sandwich.commands = ['sandwich']
+give_sandwich.priority = 'medium'
 
 
 def steveFunction1(phenny,input):
