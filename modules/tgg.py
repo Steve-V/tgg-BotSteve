8ad5884d86be1219873b63870adbdc1dbe9a974d
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-09-06 01:53:03 -0400

    bugfixes to make .element work

--- modules/tgg.py
+++ modules/tgg.py
@@ -235,36 +235,36 @@ def give_element(phenny, input):
   
   if (db_result):
     #first choose a db_result at random, then take element 1 of that result (the insult itself)
-    flavor = random.choice(db_result)[1]
+    element = random.choice(db_result)[1]
     
     phenny.say("Here you go, %s, I'm giving you 20kg. of pure %s !" % (recepient, str(element) ) )
 give_element.commands = ['element']
 give_element.priority = 'medium'
 
 
-def give_sandwhich(phenny, input):
-  #if it's the bot getting the sandwhich, or if the user is sandwhiching themselfs
+def give_sandwich(phenny, input):
+  #if it's the bot getting the sandwich, or if the user is sandwiching themselfs
   #code here
   if input.group(2):
     if input.group(3).lower() == phenny.nick.lower():
-	  return(phenny.say("For me?  Thank you!  **gloms down and does not leave a single crumb on the plate.**
+      return(phenny.say("For me?  Thank you!  **gloms down and does not leave a single crumb on the plate.**"))
     else:
-	  recepient = input.group(2) 
-	 #otherwise give specified user sandwhich
-	 db_coon = sqlite3.connect("tgg.db")
-	 db_curr = db_conn.cursor()
-	 #nick = str(input.nick)
-	 
-	 db_curr.execute( "SELLECT * FROM sandwhich_type;" )
-	 db_result = db_curr.fetchall()
-	 
+      recepient = input.group(2) 
+  #otherwise give specified user sandwich
+  db_conn = sqlite3.connect("tgg.db")
+  db_curr = db_conn.cursor()
+  #nick = str(input.nick)
+  
+  db_curr.execute( "SELLECT * FROM sandwich_type;" )
+  db_result = db_curr.fetchall()
+  
   if (db_result):
     #first choose a db_result at random, then take element 1 of that result (the insult itself)
     flavor = random.choice(db_result)[1]
-	   
-	phenny.say("Here you go, %s, I made you a %s sandwhich!" % (recepient, str(flavor) ) )
-give_sandwhich.commands = ['sandwhich']
-give_sandwhich.priority - 'medium'
+    
+  phenny.say("Here you go, %s, I made you a %s sandwich!" % (recepient, str(flavor) ) )
+give_sandwich.commands = ['sandwich']
+give_sandwich.priority = 'medium'
 
 
 def steveFunction1(phenny,input):
