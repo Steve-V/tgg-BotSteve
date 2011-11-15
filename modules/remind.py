0d5dac44a376ff90d5b87a58272365ab888742f7
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-11-15 11:23:12 -0500

    added fix for out-of-bounds reminder request

--- modules/remind.py
+++ modules/remind.py
@@ -36,42 +36,42 @@ def setup(phenny):
 
 
 scaling = {
-   'years': 365.25 * 24 * 3600, 
-   'year': 365.25 * 24 * 3600, 
-   'yrs': 365.25 * 24 * 3600, 
-   'y': 365.25 * 24 * 3600, 
+    'years': 365.25 * 24 * 3600, 
+    'year': 365.25 * 24 * 3600, 
+    'yrs': 365.25 * 24 * 3600, 
+    'y': 365.25 * 24 * 3600, 
 
-   'months': 29.53059 * 24 * 3600, 
-   'month': 29.53059 * 24 * 3600, 
-   'mo': 29.53059 * 24 * 3600, 
+    'months': 29.53059 * 24 * 3600, 
+    'month': 29.53059 * 24 * 3600, 
+    'mo': 29.53059 * 24 * 3600, 
 
-   'weeks': 7 * 24 * 3600, 
-   'week': 7 * 24 * 3600, 
-   'wks': 7 * 24 * 3600, 
-   'wk': 7 * 24 * 3600, 
-   'w': 7 * 24 * 3600, 
+    'weeks': 7 * 24 * 3600, 
+    'week': 7 * 24 * 3600, 
+    'wks': 7 * 24 * 3600, 
+    'wk': 7 * 24 * 3600, 
+    'w': 7 * 24 * 3600, 
 
-   'days': 24 * 3600, 
-   'day': 24 * 3600, 
-   'd': 24 * 3600, 
+    'days': 24 * 3600, 
+    'day': 24 * 3600, 
+    'd': 24 * 3600, 
 
-   'hours': 3600, 
-   'hour': 3600, 
-   'hrs': 3600, 
-   'hr': 3600, 
-   'h': 3600, 
+    'hours': 3600, 
+    'hour': 3600, 
+    'hrs': 3600, 
+    'hr': 3600, 
+    'h': 3600, 
 
-   'minutes': 60, 
-   'minute': 60, 
-   'mins': 60, 
-   'min': 60, 
-   'm': 60, 
+    'minutes': 60, 
+    'minute': 60, 
+    'mins': 60, 
+    'min': 60, 
+    'm': 60, 
 
-   'seconds': 1, 
-   'second': 1, 
-   'secs': 1, 
-   'sec': 1, 
-   's': 1
+    'seconds': 1, 
+    'second': 1, 
+    'secs': 1, 
+    'sec': 1, 
+    's': 1
 }
 
 periods = '|'.join(scaling.keys())
@@ -79,32 +79,35 @@ p_command = r'\.in ([0-9]+(?:\.[0-9]+)?)\s?((?:%s)\b)?:?\s?(.*)' % periods
 r_command = re.compile(p_command)
 
 def remind(phenny, input): 
-   m = r_command.match(input.bytes)
-   if not m: 
-      return phenny.reply("Sorry, didn't understand the input.")
-   length, scale, message = m.groups()
+    m = r_command.match(input.bytes)
+    if not m: 
+        return phenny.reply("Sorry, didn't understand the input.")
+    length, scale, message = m.groups()
 
-   length = float(length)
-   factor = scaling.get(scale, 60)
-   duration = length * factor
+    length = float(length)
+    factor = scaling.get(scale, 60)
+    duration = length * factor
 
-   if duration % 1: 
-      duration = int(duration) + 1
-   else: duration = int(duration)
+    if duration % 1: 
+        duration = int(duration) + 1
+    else: duration = int(duration)
 
-   t = int(time.time()) + duration
-   reminder = (input.sender, input.nick, message)
+    t = int(time.time()) + duration
+    reminder = (input.sender, input.nick, message)
 
-   try: storage[t].append(reminder)
-   except KeyError: storage[t] = [reminder]
+    try: storage[t].append(reminder)
+    except KeyError: storage[t] = [reminder]
 
-   if duration >= 60: 
-      w = ''
-      if duration >= 3600 * 12: 
-         w += time.strftime(' on %d %b %Y', time.gmtime(t))
-      w += time.strftime(' at %H:%MZ', time.gmtime(t))
-      phenny.reply('Okay, will remind%s' % w)
-   else: phenny.reply('Okay, will remind in %s secs' % duration)
+    try:
+        if duration >= 60: 
+            w = ''
+            if duration >= 3600 * 12: 
+                w += time.strftime(' on %d %b %Y', time.gmtime(t))
+            w += time.strftime(' at %H:%MZ', time.gmtime(t))
+            phenny.reply('Okay, will remind%s' % w)
+        else: phenny.reply('Okay, will remind in %s secs' % duration)
+    except ValueError:
+        phenny.reply("That's a bit unlikely, don't you think?")
 remind.commands = ['in']
 
 if __name__ == '__main__': 
