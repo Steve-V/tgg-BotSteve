43fd456b7acd414bcf71ebaa267f43bda3deb458
Steve Vaught <Steven.D.Vaught@gmail.com>
2012-04-26 22:13:18 -0400

    Extracted all of the barks from tgg.py into bark.py

---
+++ modules/bark.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""
+barks.py - The Geek Group Phenny Module
+Phenny Copyright 2008, Sean B. Palmer, inamidst.com
+This module is copyright 2011, Steven Vaught
+Licensed under the Eiffel Forum License 2.
+
+http://inamidst.com/phenny/
+"""
+
+''' Keep sqlite local to this module for now '''
+
+import random, socket
+
+def bark(phenny, input):
+    '''Manage all the one-liner barks that BotSteve contains'''
+    #figure out which bark in particular we want
+    
+    barks = {
+        "tell" : "Sorry, .tell is not yet implemented.  Use BotSteve: tell",
+        "stream" : "The Geek Group Live Web Stream can be found at: http://justin.tv/thegeekgroup ",
+        "code" : "You can download my code at https://github.com/Steve-V/tgg-BotSteve ",
+        "history" : "Channel history can be found at: http://irclog.perlgeek.de/thegeekgroup/today",
+        "log" : "Channel history can be found at: http://irclog.perlgeek.de/thegeekgroup/today",
+        "tggbug" : "Problems with the bot?  Tell BatSteve.  To report website bugs, email:  helpdesk at thegeekgroup dot org",
+        "bug" : "Problems with the bot?  Tell BatSteve.  To report website bugs, email:  helpdesk at thegeekgroup dot org",
+        "websiteproblem" : "Problems with the bot?  Tell BatSteve.  To report website bugs, email:  helpdesk at thegeekgroup dot org",
+        "errorreport" : "Problems with the bot?  Tell BatSteve.  To report website bugs, email:  helpdesk at thegeekgroup dot org",
+        "error" : "Problems with the bot?  Tell BatSteve.  To report website bugs, email:  helpdesk at thegeekgroup dot org",
+        "retort" : "Your mother's a whore."
+    }
+    
+    
+    if not input.group(1):
+        return phenny.say("Error in bark module")
+    else:
+        command = input.group(1)
+
+    try:
+        output = barks[command]
+    except:
+        return
+    
+    phenny.reply(output)
+        
+bark.commands = ['tell', 'stream', 'code', 'git', 'log', 'history', 'bug', 'tggbug', 'errorreport', 'error']
+bark.priority = 'medium'
+
+if __name__ == '__main__': 
+   print __doc__.strip()
